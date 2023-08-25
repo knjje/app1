1a75f9d9b8c4147a9eeb337a2e0f8846b7e9b592
knjje <s6303051623161@kmutnb.ac.th>
2023-08-25 15:06:23 +0700

    Add Refs-array And Refs-func
    add and edit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
-import './style.css'
+import RefsFunc from './refs-func'
+import RefsArray from './refs-array'
+// import Banner from './banner'
+// import { Header,Content,Footer } from "./func-components";
+// import Calendar from "./class-components";
+// import { Calculator2 } from "./calculator";
+// import { EventData2 } from "./event-data";
 
 function App() {
-    return ( <
-        >
-        <
-        h2 > DeveloperThai.com < /h2>  <
-        div className = "title" > reactsjkdf < /div> <
-        />
-    );
+  return (
+    <>
+    <div>
+      <RefsFunc/>
+      <RefsArray/>
+    </div>
+    </>
+  )
 }
-
-export default App;
+export default App
--- src/event-data.js
+++ src/event-data.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 
-export function EventData2() {
+export function EventData2(){
     const onClickStartStop = (ev) => {
         let t = ev.target.innerText
         let s = (t === 'Start') ? 'Stop' : 'Start'
@@ -9,14 +9,14 @@ export function EventData2() {
     const onClickAdd = (ev) => {
         let t = ev.target.innerText
         let r = eval(t)
-        alert(`${t} = ${r}`)
-        }
+        alert = (`${t} = ${r}`)
+    }
 
-        return (
+    return (
         <div style={{textAlign:'center', marginTop:20}}>
-            <button onClick={(ev) => onClickStartStop(ev)}>Start</button><br/><br/>
-            <button onClick={(ev) => onClickAdd(ev)}>10 + 20</button><br/><br/>
-            <button onClick={(ev) => onClickAdd(ev)}>30 * 40</button><br/><br/>
-        </div>
+        <button onClick={(ev) => onClickStartStop(ev)}>Start</button><br/><br/>
+        <button onClick={(ev) => onClickAdd(ev)}>10+20</button><br/><br/>
+        <button onClick={(ev) => onClickAdd(ev)}>30*40</button><br/><br/>
+    </div>
     )
 }
