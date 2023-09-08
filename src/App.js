44784f8a73c6c8b55c42bee797abfb4f9c0ae518
knjje <s6303051623161@kmutnb.ac.th>
2023-09-08 14:40:33 +0700

    Create context-header
    Create context-header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
-import RefsFunc from './refs-func'
-import RefsArray from './refs-array'
-import MessageBox from './state-func'
+import { userContext } from './context';
+import Header from "./context-header";
+// import RefsFunc from './refs-func'
+// import RefsArray from './refs-array'
+// import MessageBox from './state-func'
+
 // import Banner from './banner'
 // import { Header,Content,Footer } from "./func-components";
 // import Calendar from "./class-components";
@@ -10,13 +13,9 @@ import MessageBox from './state-func'
 
 function App() {
   return (
-    <>
-    <div>
-      <RefsFunc/>
-      <RefsArray/>
-      <MessageBox/>
-    </div>
-    </>
+    <userContext.Provider value={'Tom Jerry'}>
+    <Header/>
+    </userContext.Provider>
   )
 }
 export default App
