f0ed6d70c36cd6ebc673d53f86ffce5fb6b2a5d2
knjje <s6303051623161@kmutnb.ac.th>
2023-09-22 15:37:35 +0700

    create route3.js
    create route3.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import React from "react";
-import { userContext } from './context';
-//import Content from "./context-content";
-import Header2 from "./context-header2";
-import Content2 from "./context-content2";
-// import RefsFunc from './refs-func'
-// import RefsArray from './refs-array'
-// import MessageBox from './state-func'
-
-// import Banner from './banner'
-// import { Header,Content,Footer } from "./func-components";
-// import Calendar from "./class-components";
-// import { Calculator2 } from "./calculator";
-// import { EventData2 } from "./event-data";
+// import Router2 from "./router2";
+import Router3 from "./router3";
 
 export default function App() {
-  let [user, setUser] = React.useState('')
-  return (
-    <userContext.Provider value={[user, setUser]}>
-    <Header2/>
-    <Content2/>
-    </userContext.Provider>
-  )
+  return <Router3/>
 }
