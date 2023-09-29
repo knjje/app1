089e414ed190bd57561f97e0a24242ffa969598f
knjje <s6303051623161@kmutnb.ac.th>
2023-09-29 15:52:50 +0700

    create form-check
    create form-check

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 // import Router2 from "./router2";
 import FormText from "./form-text";
+import FormCheck from "./form-check";
 
 export default function App() {
-  return <FormText/>
+  return (<FormText/>,<FormCheck/>
+  )
 }
